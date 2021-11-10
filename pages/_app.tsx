12b00a03cca4c01f5eb8bace5a873b5c0ce63ae1
qantata <74280486+qantata@users.noreply.github.com>
2021-11-10 16:07:51 +0200

    add mui & basic css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,11 @@
+import "./_app.css";
+
+// For MUI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import React from "react";
 
 import { RoutingContext } from "../src/routing/RoutingContext";
