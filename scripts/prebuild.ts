9e52553e47a51703e266aea093b56164b91cb0a4
qantata <74280486+qantata@users.noreply.github.com>
2021-11-17 18:53:34 +0200

    replace yarn with pnpm
    closes #11

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -6,9 +6,10 @@ import { DATABASE_URL } from "../src/server/utils/constants";
 (() => {
   const env = `NODE_ENV=production`;
   const dbUrl = `DATABASE_URL=file:${DATABASE_URL}`;
-  const prismaBin = `PRISMA_MIGRATION_ENGINE_BINARY=./lib/migration-engine-debian-openssl-1.1.x`;
+  const prismaMigrationBin = `PRISMA_MIGRATION_ENGINE_BINARY=lib/migration-engine-debian-openssl-1.1.x`;
+  const prismaLibqueryBin = `PRISMA_QUERY_ENGINE_LIBRARY=lib/libquery_engine-debian-openssl-1.1.x.so.node`;
 
-  const str = `${env}\n${dbUrl}\n${prismaBin}\n`;
+  const str = `${env}\n${dbUrl}\n${prismaMigrationBin}\n${prismaLibqueryBin}\n`;
 
   fs.writeFileSync(path.join(process.cwd(), ".env"), str, {});
 })();
