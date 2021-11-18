05f3dbe1213b0dc2c3a2ff88ff8dd387f6360661
qantata <74280486+qantata@users.noreply.github.com>
2021-11-18 15:35:35 +0200

    Revert "replace yarn with pnpm"
    pnpm was having a bit of issues with nexus-prisma and prisma.
    Try to switch to pnpm again once nexus-prisma reaches 1.0.0
    
    This reverts commit 9e52553e47a51703e266aea093b56164b91cb0a4.

--- relay.config.js
+++ relay.config.js
@@ -3,5 +3,5 @@ module.exports = {
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "./src/client",
   schema: "./schema.graphql",
-  exclude: ["./src/server", "**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+  exclude: ["./src/server", "**/node_modules/**", "**/__mocks__/**", "**/__generated__/**", "**/.yarn/**"],
 }
--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -6,10 +6,9 @@ import { DATABASE_URL } from "../src/server/utils/constants";
 (() => {
   const env = `NODE_ENV=production`;
   const dbUrl = `DATABASE_URL=file:${DATABASE_URL}`;
-  const prismaMigrationBin = `PRISMA_MIGRATION_ENGINE_BINARY=lib/migration-engine-debian-openssl-1.1.x`;
-  const prismaLibqueryBin = `PRISMA_QUERY_ENGINE_LIBRARY=lib/libquery_engine-debian-openssl-1.1.x.so.node`;
+  const prismaBin = `PRISMA_MIGRATION_ENGINE_BINARY=./lib/migration-engine-debian-openssl-1.1.x`;
 
-  const str = `${env}\n${dbUrl}\n${prismaMigrationBin}\n${prismaLibqueryBin}\n`;
+  const str = `${env}\n${dbUrl}\n${prismaBin}\n`;
 
   fs.writeFileSync(path.join(process.cwd(), ".env"), str, {});
 })();
