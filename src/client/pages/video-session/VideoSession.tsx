73488795b06cb346ece0e45b9cbbcfb2c4eafbb5
qantata <74280486+qantata@users.noreply.github.com>
2021-12-04 07:50:08 +0200

    add some seeking test functionality

--- src/client/pages/video-session/VideoSession.tsx
+++ src/client/pages/video-session/VideoSession.tsx
@@ -1,7 +1,8 @@
 import Hls from "hls.js";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { VideoSessionQuery } from "./__generated__/VideoSessionQuery.graphql";
 import { graphql, PreloadedQuery, useMutation, usePreloadedQuery } from "react-relay";
+
 import { VideoSession_deleteVideoStreamSessionMutation } from "./__generated__/VideoSession_deleteVideoStreamSessionMutation.graphql";
 
 type Props = {
@@ -13,6 +14,14 @@ type Props = {
 
 const VideoSession: React.FunctionComponent<Props> = ({ prepared }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [hls, setHls] = useState<Hls | null>(null);
+  const [seekPosition, setSeekPosition] = useState(-1);
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [bufferedSegments, setBufferedSegments] = useState<number[]>([]);
+
+  // We need to update the segments inside a useEffect
+  const bufferedSegmentsRef = useRef<number[]>();
+  bufferedSegmentsRef.current = bufferedSegments;
 
   const data = usePreloadedQuery<VideoSessionQuery>(
     graphql`
@@ -48,26 +57,51 @@ const VideoSession: React.FunctionComponent<Props> = ({ prepared }) => {
   }
 
   useEffect(() => {
-    let hls: Hls | null = null;
+    if (profile?.isHls && videoRef.current) {
+      console.log("Loading new");
+      const newHls = new Hls({
+        debug: true,
+        startPosition: seekPosition,
+      });
+      newHls.attachMedia(videoRef.current);
 
-    if (profile?.isHls) {
-      hls = new Hls();
-      // @ts-ignore
-      hls.attachMedia(videoRef.current);
+      newHls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        newHls!.loadSource(`http://localhost:5004/data/session/${prepared.id}/stream/${profile!.id}/playlist`);
+        newHls!.on(Hls.Events.ERROR, (event, data) => {
+          console.error("ERROR:", data);
+        });
+        newHls!.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
+          //videoRef.current?.requestFullscreen();
+        });
 
-      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-        hls!.loadSource(`http://localhost:5004/data/session/${prepared.id}/stream/${profile!.id}/playlist`);
-        hls!.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-          videoRef.current?.requestFullscreen();
+        newHls!.on(Hls.Events.FRAG_BUFFERED, (event, data) => {
+          if (typeof data.frag.sn === "number") {
+            setBufferedSegments([...bufferedSegmentsRef.current!, data.frag.sn]);
+          }
         });
       });
+
+      videoRef.current.ontimeupdate = (event) => {
+        if (videoRef.current) {
+          setCurrentPosition(videoRef.current.currentTime);
+        }
+      };
+
+      setHls(newHls);
     }
 
     return () => {
-      if (hls) {
-        hls.destroy();
+      if (videoRef.current) {
+        videoRef.current.ontimeupdate = null;
       }
 
+      setBufferedSegments([]);
+      hls?.destroy();
+    };
+  }, [seekPosition]);
+
+  useEffect(() => {
+    return () => {
       deleteVideoStreamSession({
         variables: {
           id: prepared.id,
@@ -84,6 +118,10 @@ const VideoSession: React.FunctionComponent<Props> = ({ prepared }) => {
     };
   }, []);
 
+  const onButtonClick = () => {
+    setSeekPosition(240);
+  };
+
   if (!data.videoStreamSession) {
     console.error("Video stream session not found");
     return null;
@@ -95,9 +133,13 @@ const VideoSession: React.FunctionComponent<Props> = ({ prepared }) => {
   }
 
   return (
-    <video ref={videoRef} controls autoPlay style={{ maxWidth: "100%" }}>
-      {!profile.isHls && <source type="video/mp4" src={`http://localhost:5004/data/session/${prepared.id}/direct`} />}
-    </video>
+    <>
+      <video ref={videoRef} controls autoPlay style={{ width: "100%", maxWidth: "100%", aspectRatio: "16 / 9" }}>
+        {!profile.isHls && <source type="video/mp4" src={`http://localhost:5004/data/session/${prepared.id}/direct`} />}
+      </video>
+
+      <button onClick={onButtonClick}>Seek to 240</button>
+    </>
   );
 };
 
