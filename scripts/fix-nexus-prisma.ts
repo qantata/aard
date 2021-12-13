64b358efc9a3d5159e67539b27037da92c9a2c21
qantata <74280486+qantata@users.noreply.github.com>
2021-12-13 20:37:21 +0200

    fix nexus-prisma import paths for packaged app

--- scripts/fix-nexus-prisma.ts
+++ scripts/fix-nexus-prisma.ts
@@ -14,16 +14,16 @@ import fs from "fs";
       "GraphQLDocs": true,
       "JSDoc": true
     },
-    "prismaClientImportId": "../../../.prisma/client"
+    "prismaClientImportId": "/snapshot/aard/node_modules/.prisma/client"
   }
   
   const dmmf = getPrismaClientDmmf({
     // JSON stringify the values to ensure proper escaping
     // Details: https://github.com/prisma/nexus-prisma/issues/143
     // TODO test that fails without this code
-    require: () => require("../../../.prisma/client"),
+    require: () => require("/snapshot/aard/node_modules/.prisma/client"),
     importId: gentimeSettings.prismaClientImportId,
-    importIdResolved: require.resolve("../../../.prisma/client")
+    importIdResolved: require.resolve("/snapshot/aard/node_modules/.prisma/client")
   })
   
   const models = ModelsGenerator.JS.createNexusTypeDefConfigurations(dmmf, {
