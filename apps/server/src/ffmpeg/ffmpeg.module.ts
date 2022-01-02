4d9c36318b6384defb44a156192ee9f27be09024
qantata <74280486+qantata@users.noreply.github.com>
2022-01-02 02:44:13 +0200

    add ffmpeg cache methods to FFmpegModule

--- apps/server/src/ffmpeg/ffmpeg.module.ts
+++ apps/server/src/ffmpeg/ffmpeg.module.ts
@@ -3,14 +3,6 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import * as path from "path";
 import * as fs from "fs";
 
-import {
-  getDecoders,
-  getDemuxerFileExtensions,
-  getDemuxers,
-  getEncoders,
-  getMuxers,
-  setFfmpegSupportValues,
-} from "@/utils/ffmpeg-support";
 import { FFmpegService } from "./ffmpeg.service";
 import { Env } from "@/config/env";
 import { FilesystemService } from "@/filesystem/filesystem.service";
@@ -18,9 +10,10 @@ import { FilesystemModule } from "@/filesystem/filesystem.module";
 
 const ffmpegFactory = {
   provide: "FFMPEG_CACHE",
-  inject: [ConfigService, FilesystemService],
-  useFactory: async (config: ConfigService<Env>, filesystem: FilesystemService) => {
+  inject: [FFmpegService, ConfigService, FilesystemService],
+  useFactory: async (ffmpeg: FFmpegService, config: ConfigService<Env>, filesystem: FilesystemService) => {
     return new Promise<void>(async (resolve) => {
+      // TODO: Don't hardcode path
       const cachePath = path.join(config.get("DATA_DIR"), "ffmpeg-cache.json");
       const ffmpegHash: string = await filesystem.sha256("/usr/bin/ffmpeg");
 
@@ -37,7 +30,6 @@ const ffmpegFactory = {
         );
 
         try {
-          // TODO: Don't hardcode path
           const oldHash = cacheData.ffmpegHash;
 
           if (!oldHash || ffmpegHash !== oldHash) {
@@ -69,11 +61,11 @@ const ffmpegFactory = {
         console.log("Updating ffmpeg cache, this will take a moment...");
 
         // Update to cache is needed, fetch values from ffmpeg and write them to the cache
-        const decoders = await getDecoders();
-        const encoders = await getEncoders();
-        const demuxers = await getDemuxers();
-        const muxers = await getMuxers();
-        const extensions = await getDemuxerFileExtensions();
+        const decoders = await ffmpeg.getDecoders();
+        const encoders = await ffmpeg.getEncoders();
+        const demuxers = await ffmpeg.getDemuxers();
+        const muxers = await ffmpeg.getMuxers();
+        const extensions = await ffmpeg.getDemuxerFileExtensions();
 
         const data = {
           ffmpegHash,
@@ -95,7 +87,7 @@ const ffmpegFactory = {
         });
       } else {
         // Set values so that they don't need to be fetched from ffmpeg - the entire point of the cache!
-        setFfmpegSupportValues(
+        ffmpeg.setSupportValues(
           cacheData.decoders,
           cacheData.encoders,
           cacheData.demuxers,
--- apps/server/src/ffmpeg/ffmpeg.service.ts
+++ apps/server/src/ffmpeg/ffmpeg.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { spawn } from "child_process";
 
-import { FilesystemService } from "@/filesystem/filesystem.service";
-import { ConfigService } from "@nestjs/config";
-import { Env } from "@/config/env";
-
 export type FFProbeOutputType = {
   streams: {
     index?: number;
@@ -27,11 +23,241 @@ export type FFProbeOutputType = {
   };
 };
 
+type Coders = {
+  video: {
+    [key: string]: boolean;
+  };
+  audio: {
+    [key: string]: boolean;
+  };
+  subtitle: {
+    [key: string]: boolean;
+  };
+};
+
+type Muxers = {
+  [key: string]: boolean;
+};
+
 @Injectable()
 export class FFmpegService {
-  constructor(private filesystem: FilesystemService, private config: ConfigService<Env>) {}
+  private decoders: Coders | null = null;
+  private encoders: Coders | null = null;
+  private demuxers: Muxers | null = null;
+  private muxers: Muxers | null = null;
+  private demuxerFileExtensions: string[] | null = null;
 
-  private async updateFFmpegCache() {}
+  setSupportValues(decoders: Coders, encoders: Coders, demuxers: Muxers, muxers: Muxers, extensions: string[]) {
+    this.decoders = decoders;
+    this.encoders = encoders;
+    this.demuxers = demuxers;
+    this.muxers = muxers;
+    this.demuxerFileExtensions = extensions;
+  }
+
+  private getCodersFromFFmpegOutput(output: string[]) {
+    const coders: Coders = {
+      video: {},
+      audio: {},
+      subtitle: {},
+    };
+
+    for (const coder of output) {
+      const cols = coder.split(" ");
+
+      if (cols.length < 3) {
+        throw new Error("ffmpeg encoder/decoder output was unexpected; unsupported version?");
+      }
+
+      const flags = cols[1];
+      const codecs = [cols[2]];
+
+      // Sometimes there's an additional codec name at the
+      // end of the line
+      if (cols[cols.length - 2] === "(codec") {
+        codecs.push(cols[cols.length - 1].replace(")", ""));
+      }
+
+      for (const codec of codecs) {
+        switch (flags[0]) {
+          case "V":
+            coders.video[codec] = true;
+            break;
+          case "A":
+            coders.audio[codec] = true;
+            break;
+          case "S":
+            coders.subtitle[codec] = true;
+            break;
+          default:
+            console.warn("encountered unknown ffmpeg type:", flags[0]);
+        }
+      }
+    }
+
+    return coders;
+  }
+
+  private getMuxersFromFFmpegOutput(output: string[]) {
+    const muxers: Muxers = {};
+
+    for (const muxer of output) {
+      const cols = muxer.split(" ");
+
+      if (cols.length < 4) {
+        throw new Error("ffmpeg muxer/demuxer output was unexpected; unsupported version?");
+      }
+
+      muxers[cols[3]] = true;
+    }
+
+    return muxers;
+  }
+
+  private getRelevantLinesFromFFmpegOutput(output: string) {
+    const dividerIndex = output.indexOf("-\n");
+
+    if (dividerIndex === -1) {
+      throw new Error("ffmpeg output was unexpected; unsupported version?");
+    }
+
+    let result = output.slice(dividerIndex, output.length).split("\n");
+    // First row is the divider, last row is an empty row. Other rows are actual data
+    if (result.length <= 2) {
+      return null;
+    }
+
+    return (result = result.slice(1, result.length - 1));
+  }
+
+  async getDecoders(): Promise<Coders | null> {
+    if (this.decoders !== null) {
+      return this.decoders;
+    }
+
+    try {
+      const result = await this.ffmpeg(["-decoders"]);
+
+      const decoders = this.getRelevantLinesFromFFmpegOutput(result);
+      if (!decoders) {
+        return null;
+      }
+
+      this.decoders = this.getCodersFromFFmpegOutput(decoders);
+      return this.decoders;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async getEncoders(): Promise<Coders | null> {
+    if (this.encoders !== null) {
+      return this.encoders;
+    }
+
+    try {
+      const result = await this.ffmpeg(["-encoders"]);
+
+      const encoders = this.getRelevantLinesFromFFmpegOutput(result);
+      if (!encoders) {
+        return null;
+      }
+
+      this.encoders = this.getCodersFromFFmpegOutput(encoders);
+      return this.encoders;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async getDemuxers(): Promise<Muxers | null> {
+    if (this.demuxers !== null) {
+      return this.demuxers;
+    }
+
+    try {
+      const result = await this.ffmpeg(["-demuxers"]);
+
+      const demuxers = this.getRelevantLinesFromFFmpegOutput(result);
+      if (!demuxers) {
+        return null;
+      }
+
+      this.demuxers = this.getMuxersFromFFmpegOutput(demuxers);
+      return this.demuxers;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async getMuxers(): Promise<Muxers | null> {
+    if (this.muxers !== null) {
+      return this.muxers;
+    }
+
+    try {
+      const result = await this.ffmpeg(["-muxers"]);
+
+      const muxers = this.getRelevantLinesFromFFmpegOutput(result);
+      if (!muxers) {
+        return null;
+      }
+
+      this.muxers = this.getMuxersFromFFmpegOutput(muxers);
+      return this.muxers;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async getDemuxerFileExtensions(): Promise<string[] | null> {
+    if (this.demuxerFileExtensions !== null) {
+      return this.demuxerFileExtensions;
+    }
+
+    try {
+      const demuxers = await this.getDemuxers();
+      if (!demuxers) {
+        return null;
+      }
+
+      let extensions: string[] = [];
+      for (let [demuxer, _] of Object.entries(demuxers)) {
+        try {
+          const result = await this.ffmpeg(["-h", `demuxer=${demuxer}`]);
+          const commonExtensionsStr = "Common extensions: ";
+          const commonExtensionsStrIndex = result.indexOf(commonExtensionsStr);
+
+          if (commonExtensionsStrIndex === -1) {
+            continue;
+          }
+
+          // Find start and end index for extension list
+          const startIdx = commonExtensionsStrIndex + commonExtensionsStr.length;
+          let endIdx = startIdx;
+          for (let idx = startIdx; idx < result.length; idx++) {
+            if (result[idx] === ".") {
+              endIdx = idx;
+              break;
+            }
+          }
+
+          extensions = [...extensions, ...result.substring(startIdx, endIdx).split(",")];
+        } catch (err) {
+          console.error(err);
+        }
+      }
+
+      return extensions;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
 
   private async ffspawn(cmd: "ffmpeg" | "ffprobe", filepath: string | null, args: string[]) {
     return new Promise<string>((resolve, reject) => {
